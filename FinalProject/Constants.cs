55a0d01570995fd027b91e02188ce8193dfefa03
BobJohn69 <minhtuan030304@gmail.com>
2025-02-25 20:26:07 +0700

    delete constants

--- FinalProject/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FinalProject.Constants
-{
-    public static class Constants
-    {
-        public const string AuthScheme = "ap-auth";
-        public const string AuthCookie = "auth_token";
-    }
-}
