9dceb8f2d5cf03e8ff46e8c7bc86ccb239b216f9
BobJohn69 <minhtuan030304@gmail.com>
2025-03-16 16:48:08 +0700

    Add payment

---
+++ Repository/Interface/IPaymentRepository.cs
@@ -0,0 +1,16 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interface
+{
+    public interface IPaymentRepository
+    {
+        Task AddPaymentAsync(Payment payment);
+        Task<IEnumerable<Payment>> GetPaymentsAsync(DateTime startDate, DateTime endDate);
+
+    }
+}
